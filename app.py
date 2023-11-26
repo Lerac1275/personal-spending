df16f835b1c3db32cfee3a588645b2d4433b0d2c
Lerac1275 <carelchay@outlook.com>
2023-11-26 17:27:06 +0800

    Updated date handling
    Added functions to handle date splicing. Also improved the data loading portion.
    Now data is refreshed only when button is clicked (excluding on startup)

--- app.py
+++ app.py
@@ -2,16 +2,37 @@
 
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+import pandas as pd
 
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
+@st.cache_data #input is a placeholder to be used when we want to re-pull data
+def get_data(input):
+    # Create a connection object.
+    conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read(
-    worksheet="Combined",
-    ttl="10m", # Cache will expire in 10 minutes
-    usecols=[0, 2, 3, 4, 5, 6, 7, 8, 9],
-    # nrows=3,
-)
+    df = conn.read(
+        worksheet="Combined",
+        ttl="0m", # Cache will expire in o minutes
+        usecols=[0, 2, 3, 4, 5, 6, 7, 8, 9],
+        # nrows=3,
+    )
+    # Remove NA rows
+    df = df.loc[~df.Date.isna()]
+    # Convert to date type
+    df.Date=pd.to_datetime(df.Date)
+    # Add the month
+    df['month']=df['Date'].to_numpy().astype('datetime64[M]')
+    # Add the week (start of the week is every monday)
+    df['week_start'] = df['Date'].dt.to_period('W').apply(lambda r: r.start_time)
+    return df
+
+if "input" not in st.session_state:
+    st.session_state['input']=1
+
+refresh_data = st.button(label="Refresh Raw Data", type='primary')
+if refresh_data:
+    st.session_state.input += 1
+
+df = get_data(st.session_state.input)
 
 df.to_pickle("./data/sample_data.pkl")
 
--- appUtilities.py
+++ appUtilities.py
@@ -7,8 +7,26 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+import datetime
 import sys
 
+# Generates today's date (returns string of YYYY-MM-DD by default)
+def get_today(string=True):
+    return datetime.datetime.now().strftime("%Y-%m-%d")
+
 # Slices a dataframe according to the given dates (inclusive)
-def get_dates(df:pd.DataFrame, start_date, end_date, date_column:str='Date')->pd.DataFrame:
+def get_dates(df:pd.DataFrame, start_date:str, end_date:str, date_column:str='Date')->pd.DataFrame:
     return df[(df[date_column]>=start_date)&(df[date_column]<=end_date)]
+
+# Get relevant data for that month
+def get_month(df:pd.DataFrame, month):
+    return df.loc[df.month==month]
+
+# Get relevant data for that week
+def get_week(df:pd.DataFrame, week:str):
+    # Get the following week Monday's date. This will be a dt obj
+    # week_end = datetime.datetime.strptime(week, '%Y-%m-%d') + datetime.timedelta(days=7)
+    if datetime.datetime.strptime(week, '%Y-%m-%d').strftime("%a") != 'Mon':
+        raise Exception(f"week of {week} was not a Monday")
+    else:
+        return df.loc[df.week_start==week]
