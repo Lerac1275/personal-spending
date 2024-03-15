feb608ada90e91ac1871cdf0075f0347943c4a3e
Lerac1275 <carelchay@outlook.com>
2024-03-15 16:57:57 +0800

    Added functionality to dashboard
    Additions to the table and donut chart

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 import appUtilities as au
 
-@st.cache_data()
-def dummy_data(): # A helper function for prototyping, reads in a dummy dataframe to avoid having to re-pull data during prototyping
-    return pd.read_pickle('./data/dummy_data.pkl')
+@st.cache_data
+def dummy_data(path = "./data/dummy_data.pkl"): # A helper function for prototyping, reads in a dummy dataframe to avoid having to re-pull data during prototyping
+    df = pd.read_pickle(path)
+    return df
 
 @st.cache_data #input is a placeholder to be used when we want to re-pull data
 def get_data(input):
@@ -24,16 +25,21 @@ def get_data(input):
     df = df.loc[~df.Date.isna()]
     # Convert to date type
     df.Date=pd.to_datetime(df.Date)
-    # Add the month
+    # Add the month. This will turn any date to the FIRST DAY of that month. E.g. 2024-02-17 - 2024-02-01
     df['month']=df['Date'].to_numpy().astype('datetime64[M]')
     # Add the week (start of the week is every monday)
     df['week_start'] = df['Date'].dt.to_period('W').apply(lambda r: r.start_time)
     return df
 
+st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="collapsed", menu_items=None)
+
 if "input" not in st.session_state:
     st.session_state['input']=1
 # Ensures that data is only pulled from google API when the button is clicked (aside from when the app is first run. )
-refresh_data = st.button(label="Refresh Raw Data", type='primary')
+    
+with st.sidebar:
+    refresh_data = st.button(label="Refresh Raw Data", type='primary')
+
 if refresh_data:
     st.session_state.input += 1
 
@@ -42,11 +48,18 @@ df = dummy_data()
 
 # df.to_pickle("./data/dummy_data.pkl")
 
-st.dataframe(df)
-st.dataframe(df.dtypes)
+# Commented out for tidinesss
+# st.dataframe(df)
+# st.dataframe(df.dtypes)
 
 styled_df = au.create_main_table(df=df, reverse=False)
-st.dataframe(styled_df)
+
+main_col, rightbar = st.columns([3, 1])
+with main_col:
+    with st.container(border=True):
+        st.dataframe(styled_df)        
+        figure = au.pie_chart_most_recent(df)
+        st.plotly_chart(figure, use_container_width=True)
 
 # Print results.
 # for row in df.itertuples():
--- appUtilities.py
+++ appUtilities.py
@@ -3,6 +3,7 @@ Helper file to be called in the main streamlit app.
 
 Contains functions for slicing data & graphing
 """
+import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
@@ -10,28 +11,53 @@ import plotly.graph_objects as go
 import datetime
 import sys, pytz
 
-# Generates today's date (returns string of YYYY-MM-DD by default)
-def get_today(string=True):
-    return datetime.datetime.now().strftime("%Y-%m-%d")
+def get_endoff_day():
+    """
+    Returns the datetime of the end of the CURRENT day. Intended to be used in the form < so this returns the start of the next day (from today)
+    """
+   # Get Singapore's timezone
+    sg_timezone = pytz.timezone('Asia/Singapore')
 
-# Slices a dataframe according to the given dates (inclusive)
-def get_dates(df:pd.DataFrame, start_date:str, end_date:str, date_column:str='Date')->pd.DataFrame:
-    return df[(df[date_column]>=start_date)&(df[date_column]<=end_date)]
+    # Get the current date and time in Singapore's timezone
+    current_date_sg = datetime.datetime.now(sg_timezone)
 
-# Get relevant data for that week
-def get_week(df:pd.DataFrame, week:str):
-    # Get the following week Monday's date. This will be a dt obj
-    # week_end = datetime.datetime.strptime(week, '%Y-%m-%d') + datetime.timedelta(days=7)
-    if datetime.datetime.strptime(week, '%Y-%m-%d').strftime("%a") != 'Mon':
-        raise Exception(f"week of {week} was not a Monday")
-    else:
-        return df.loc[df.week_start==week]
+    return pd.to_datetime(current_date_sg.date() + pd.DateOffset(days=1))
+
+def get_current_month():
+    """
+    Returns the first day of the CURRENT month (without timezone)
+    """
+    # Get Singapore's timezone
+    sg_timezone = pytz.timezone('Asia/Singapore')
+
+    # Get the current date and time in Singapore's timezone
+    current_date_sg = datetime.datetime.now(sg_timezone)
+
+    # Subtract n months from the current date
+    n_months_ago = current_date_sg - pd.DateOffset(months=0)
+    # Set the day of the resulting date to 1 to get the first day of the month
+    n_months_ago = n_months_ago.replace(day=1).date()
+    # Return datetime timestamp type
+    return pd.to_datetime(n_months_ago)
     
-
+# @st.cache_data(show_spinner=True)This cannot be styled
 def create_main_table(df:pd.DataFrame, n_months = 4, reverse=False):
     """
     Displays the main landing page table, which is the spending by type for the last 4 months
+
+    Parameters
+    ----------
+    df:pd.DataFrame
+        Pandas dataframecontaining individual purchase records. 
+    n_months: int
+        number of months back to go
+    reverse: bool, False by default
+        Whether months are in DESCENDING order (from left to right). reverse=False means columns will for example be Jan, Feb, Mar, Apr. reverse=True means they will be Apr, Mar, Feb, Jan. Handles color reformatting as well. 
+        
     """
+
+    n_months = n_months-1 #since zero-indexed
+
     # Get Singapore's timezone
     sg_timezone = pytz.timezone('Asia/Singapore')
 
@@ -43,8 +69,9 @@ def create_main_table(df:pd.DataFrame, n_months = 4, reverse=False):
     # Set the day of the resulting date to 1 to get the first day of the month
     n_months_ago = n_months_ago.replace(day=1).date()
 
-    # filter entries
-    tdf = df.loc[df['Date']>= pd.to_datetime(n_months_ago)].copy()
+    # filter entries between the last n_months and today's date
+    tdf = df.loc[(df['Date']>= pd.to_datetime(n_months_ago))
+                 & (df['Date'] <= get_endoff_day())].copy()
     # Group by month & type
     tdf = tdf.groupby(['month', 'Type']).Amount.sum()
     # Pivot long to wide
@@ -55,7 +82,7 @@ def create_main_table(df:pd.DataFrame, n_months = 4, reverse=False):
     column_sums = tdf.sum()
     column_sums.name = "Total" # Set the name of the series
     # Append sums as a new row to the DataFrame
-    tdf = pd.concat([pd.DataFrame(column_sums).T, tdf]) #Add the column sums so they appear at the top
+    tdf = pd.concat([tdf, pd.DataFrame(column_sums).T]) #Add the column sums so they appear at the top
     # Set the index name
     tdf.index.name = "Type"
     # Change the date columns
@@ -76,7 +103,7 @@ def create_main_table(df:pd.DataFrame, n_months = 4, reverse=False):
             elif s[i] < s[i - 1]:
                 styles.append(f'background-color: {"green" if not reverse else "red"}')
             else:
-                styles.append('background-color: blue, ')  # Same value
+                styles.append('background-color: blue')  # Same value
 
         if reverse:
             styles.append('background-color: grey')
@@ -89,3 +116,32 @@ def create_main_table(df:pd.DataFrame, n_months = 4, reverse=False):
     styled_df = tdf.style.format("${:,.2f}").apply(highlight_left, axis = 1)
     # Display styled DataFrame
     return styled_df
+
+def pie_chart_most_recent(df:pd.DataFrame):
+    """
+    Makes a pie chart of spending for the most recent month
+    """
+    tdf = df.loc[(df.month>= get_current_month())
+            & (df.Date<get_endoff_day())]
+    tdf = tdf.groupby('Type').agg(
+        total=pd.NamedAgg(column='Amount', aggfunc='sum')
+    ).reset_index().sort_values(by='Type')
+
+    # Create a donut chart using Plotly Graph Objects
+    fig = go.Figure()
+    values = tdf.total.values
+    labels = tdf.Type.values
+    fig.add_trace(go.Pie(
+        labels=labels,
+        values=values,
+        hole=0.5,  # Set the hole parameter to create a donut chart
+        marker=dict(colors=['red', 'green', 'blue', 'orange'])  # You can customize colors if needed
+    ))
+    total_spending = sum(values)
+    fig.update_layout(
+        # title='Donut Chart Example',
+        annotations=[dict(text=f'${total_spending:,.0f}', x=0.5, y=0.5, font_size=20, showarrow=False)]
+        , legend= dict(x = 0.3, y=-0.1, orientation='h')
+    )
+
+    return fig
